5bd3eb9d771b78e3c9c31be05dbd01e74645ac5c
Victor Ome <vidahoro@gmail.com>
2022-05-20 22:34:03 -0500

    rom_test program verification

--- rom_test/rom_test.vhd
+++ rom_test/rom_test.vhd
@@ -4,9 +4,9 @@ use IEEE.numeric_std.all;
 
 
 entity rom_test is
-  port(clock    :in std_logic;
-       address  :in std_logic_vector(7 downto 0);
-       hex0, hex1 :out std_logic_vector(0 to 6));
+  port(CLOCK_50   :in std_logic;
+       SW  :in std_logic_vector(7 downto 0);
+       HEX0, HEX1 :out std_logic_vector(6 downto 0));
 end entity;
 
 
@@ -16,12 +16,8 @@ architecture arch_rom_test of rom_test is
 	-- Declarations (optional)
 	component parte_1 is
 		port( SW : in std_logic_vector (3 downto 0);
-		 SEG: out std_logic_vector (6 downto 0);
-			k : out std_logic;
-			r : out std_logic;
-			s : out std_logic;
-			t : out std_logic
-		
+		 HEX0: out std_logic_vector (6 downto 0)
+			
 		);
 		
 	end component;
@@ -40,9 +36,9 @@ architecture arch_rom_test of rom_test is
 
 begin
 
-	rom : rom_128x8_sync port map (clock, address, data_out);
-	S0 : parte_1 port map (data_out1, hex0);
-	S1 : parte_1 port map (data_out2,hex1);	
+	rom : rom_128x8_sync port map (CLOCK_50, SW, data_out);
+	S0 : parte_1 port map (data_out1,HEX0);
+	S1 : parte_1 port map (data_out2,HEX1);	
 
 
 
